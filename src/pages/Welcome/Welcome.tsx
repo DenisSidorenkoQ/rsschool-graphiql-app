28b608c90a2d698cf3be0fe942f93d05289c216f
alex2075 <gagara5620@gmail.com>
2023-05-08 11:54:14 +0600

    feat: add redirect by button

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,39 +1,143 @@
+import { Col, Image, Layout, Row, Button, theme, Space } from 'antd';
+import Paragraph from 'antd/es/typography/Paragraph';
+import Title from 'antd/es/typography/Title';
 import React from 'react';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import seminar from '../../assets/Seminar-rafiki.png';
+import landing from '../../assets/landing.png';
+import optimization from '../../assets/optimization.png';
+import workflow from '../../assets/workflow.png';
+import { useNavigate } from 'react-router-dom';
 
 const { Header, Content, Footer } = Layout;
 
-const App: React.FC = () => {
-    const {
-        token: { colorBgContainer },
-    } = theme.useToken();
+export const Welcome: React.FC = () => {
+  const tokenAuth = localStorage.getItem('token');
+  const navigate = useNavigate();
 
-    return (
-        <Layout className="layout">
-            <Header>
-                <div className="logo" />
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    defaultSelectedKeys={['2']}
-                    items={new Array(15).fill(null).map((_, index) => {
-                        const key = index + 1;
-                        return {
-                            key,
-                            label: `nav ${key}`,
-                        };
-                    })}
-                />
-            </Header>
-            <Content style={{ padding: '0 50px' }}>
-                <div className="site-layout-content" style={{ background: colorBgContainer }}>
-                    Content
-                </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
-        </Layout>
-    );
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout className="layout">
+      <Header
+        style={{
+          display: 'flex',
+          justifyContent: 'end',
+          alignItems: 'center',
+          columnGap: '8px',
+        }}
+      >
+        {tokenAuth ? (
+          <Button
+            ghost
+            onClick={() => {
+              navigate('/main');
+            }}
+          >
+            Go to Main Page
+          </Button>
+        ) : (
+          <Space>
+            <Button
+              ghost
+              onClick={() => {
+                navigate('/sign');
+              }}
+            >
+              Sign In
+            </Button>
+            <Button
+              ghost
+              onClick={() => {
+                navigate('/sign');
+              }}
+            >
+              Sign Up
+            </Button>
+          </Space>
+        )}
+      </Header>
+      <Content
+        style={{
+          padding: '0 50px',
+          background: colorBgContainer,
+        }}
+      >
+        <Title level={3} style={{ textAlign: 'center', padding: '40px' }}>
+          Welcome to our project page!
+        </Title>
+
+        <Row justify="space-around" align="middle">
+          <Col style={{ textAlign: 'center' }} xs={24} sm={24} md={12} lg={12}>
+            <Image width={300} src={seminar} preview={false} />
+          </Col>
+          <Col xs={24} sm={24} md={12} lg={12}>
+            <Paragraph strong style={{ textAlign: 'center' }}>
+              We are{' '}
+              <a href="https://github.com/DenisSidorenkoQ" target="_blank" rel="noreferrer">
+                DenisSidorenkoQ
+              </a>
+              ,{' '}
+              <a href="https://github.com/kalinkinfiz" target="_blank" rel="noreferrer">
+                KalinkinFiz
+              </a>
+              , and{' '}
+              <a href="https://github.com/alexandr2075" target="_blank" rel="noreferrer">
+                Alexandr2075
+              </a>
+              , and we are excited to present our latest project: a GraphiQL playground/IDE for
+              GraphQL requests.
+            </Paragraph>
+          </Col>
+        </Row>
+
+        <Row justify="space-around" align={'middle'}>
+          <Col xs={24} sm={24} md={12} lg={14}>
+            <Paragraph strong style={{ textAlign: 'center' }}>
+              Our team consists of experienced developers with a passion for creating innovative
+              solutions to complex problems. We believe that GraphQL is the future of APIs, and we
+              are committed to building tools that make it easier for developers to work with this
+              powerful technology.
+            </Paragraph>
+          </Col>
+          <Col style={{ textAlign: 'center' }} xs={24} sm={24} md={12} lg={10}>
+            <Image width={300} src={optimization} preview={false} />
+          </Col>
+        </Row>
+
+        <Row justify="space-around" align="middle">
+          <Col style={{ textAlign: 'center' }} xs={24} sm={24} md={12} lg={12}>
+            <Image width={300} src={landing} preview={false} />
+          </Col>
+          <Col xs={24} sm={24} md={12} lg={12}>
+            <Paragraph strong style={{ textAlign: 'center' }}>
+              This project is part of the RS SCHOOL React Course, an intensive training program
+              designed to help developers master the latest front-end technologies. As part of this
+              program, we have been working hard to create a tool that will help developers of all
+              levels to experiment with GraphQL queries and learn more about this exciting
+              technology.
+            </Paragraph>
+          </Col>
+        </Row>
+
+        <Row justify="space-around" align={'middle'}>
+          <Col xs={24} sm={24} md={12} lg={14}>
+            <Paragraph strong style={{ textAlign: 'center' }}>
+              Whether you are an experienced GraphQL developer or just getting started with this
+              powerful tool, we believe that our GraphiQL playground/IDE will be a valuable addition
+              to your toolkit. So take a look around, try out some queries, and let us know what you
+              think!
+            </Paragraph>
+          </Col>
+          <Col style={{ textAlign: 'center' }} xs={24} sm={24} md={12} lg={10}>
+            <Image width={300} src={workflow} preview={false} />
+          </Col>
+        </Row>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        <div className="site-layout-footer">Footer</div>
+      </Footer>
+    </Layout>
+  );
 };
-
-export default App;
-
