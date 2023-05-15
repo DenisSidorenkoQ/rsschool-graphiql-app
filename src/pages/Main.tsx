ecd901171a9c4a5e3e7dbc361d7e9916f722d308
Denis <apricot103@mail.ru>
2023-05-15 15:40:45 +0300

    feat: code editor

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import CodeEditor from '@uiw/react-textarea-code-editor';
 
 export const Main = () => {
+    const [code, setCode] = React.useState(
+        `function add(a, b) {\n  return a + b;\n}`
+    );
+
   return (
     <>
-      <span>Main page</span>
+        <div style={{}}>
+            <CodeEditor
+                value={code}
+                language="js"
+                placeholder="Please enter GraphQL"
+                onChange={(evn) => setCode(evn.target.value)}
+                padding={10}
+                style={{
+                    fontSize: 14,
+                    backgroundColor: "gray",
+                    fontFamily:
+                        "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace"
+                }}
+            />
+        </div>
+
     </>
   );
 };
