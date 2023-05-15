3ce4f056d5778c4194ad9133d053cf9a51f7f3f7
Denis <apricot103@mail.ru>
2023-05-15 15:34:57 +0300

    fix: fix sing up

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import CodeEditor from '@uiw/react-textarea-code-editor';
 
 export const Main = () => {
+    const [code, setCode] = React.useState(
+        `function add(a, b) {\n  return a + b;\n}`
+    );
+
   return (
     <>
-      <span>Main page</span>
+        <CodeEditor
+            value={code}
+            language="js"
+            placeholder="Please enter GraphQL"
+            onChange={(evn) => setCode(evn.target.value)}
+            padding={10}
+            style={{
+                fontSize: 12,
+                backgroundColor: "gray",
+                fontFamily:
+                    "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace"
+            }}
+        />
     </>
   );
 };
