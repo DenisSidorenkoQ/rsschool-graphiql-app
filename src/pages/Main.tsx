515f65bda6edd1510c864f6f0f5b63f328bcf29a
Denis <apricot103@mail.ru>
2023-05-15 15:43:52 +0300

    fix: main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,26 +1,9 @@
 import React from 'react';
-import CodeEditor from '@uiw/react-textarea-code-editor';
 
 export const Main = () => {
-    const [code, setCode] = React.useState(
-        `function add(a, b) {\n  return a + b;\n}`
-    );
-
   return (
     <>
-        <CodeEditor
-            value={code}
-            language="js"
-            placeholder="Please enter GraphQL"
-            onChange={(evn) => setCode(evn.target.value)}
-            padding={10}
-            style={{
-                fontSize: 12,
-                backgroundColor: "gray",
-                fontFamily:
-                    "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace"
-            }}
-        />
+        <span>Main page</span>
     </>
   );
 };
