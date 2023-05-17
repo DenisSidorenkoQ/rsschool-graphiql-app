993ba401ea5b1413a9c6380337c8a3d0d9695531
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-05-18 00:17:09 +0300

    feat: add LangContext.Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Welcome } from './pages/Welcome/Welcome';
-import './App.css';
-import { PageHeader } from './components/PageHeader';
-import { Main } from './pages/Main';
-import { PageFooter } from './components/PageFooter';
-import { Error } from './pages/Error/Error';
 import { useAuthState } from 'react-firebase-hooks/auth';
+
+import './App.css';
+import { Welcome } from './pages/Welcome/Welcome';
+import { Main } from './pages/Main';
+import { Error } from './pages/Error/Error';
+
+import { PageHeader } from './components/PageHeader';
+import { PageFooter } from './components/PageFooter';
+
 import { auth } from './firebase';
+import { LangContext } from './context/lang';
 
 const App = () => {
+  const [lang, setLang] = React.useState(localStorage.getItem('lang') || 'ru');
+
+  const toggleLang = () => {
+    const newLang = lang === 'ru' ? 'en' : 'ru';
+
+    setLang(newLang);
+    localStorage.setItem('lang', newLang);
+  };
+
   const [user, loading] = useAuthState(auth);
 
   if (loading) {
@@ -18,7 +32,40 @@ const App = () => {
   if (user?.uid) {
     return (
       <div className="App">
+        <LangContext.Provider value={{ language: lang, toggleLanguage: toggleLang }}>
+          <Routes>
+            <Route
+              path={'/'}
+              element={
+                <PageHeader>
+                  <PageFooter>
+                    <Welcome />
+                  </PageFooter>
+                </PageHeader>
+              }
+            />
+            <Route
+              path={'/main'}
+              element={
+                <PageHeader>
+                  <PageFooter>
+                    <Main />
+                  </PageFooter>
+                </PageHeader>
+              }
+            />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </LangContext.Provider>
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <LangContext.Provider value={{ language: lang, toggleLanguage: toggleLang }}>
         <Routes>
+          <Route path="*" element={<Error />} />
           <Route
             path={'/'}
             element={
@@ -29,37 +76,8 @@ const App = () => {
               </PageHeader>
             }
           />
-          <Route
-            path={'/main'}
-            element={
-              <PageHeader>
-                <PageFooter>
-                  <Main />
-                </PageFooter>
-              </PageHeader>
-            }
-          />
-          <Route path="*" element={<Error />} />
         </Routes>
-      </div>
-    );
-  }
-
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="*" element={<Error />} />
-        <Route
-          path={'/'}
-          element={
-            <PageHeader>
-              <PageFooter>
-                <Welcome />
-              </PageFooter>
-            </PageHeader>
-          }
-        />
-      </Routes>
+      </LangContext.Provider>
     </div>
   );
 };
