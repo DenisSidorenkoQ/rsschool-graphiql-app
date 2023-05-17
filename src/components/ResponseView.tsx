20cafac36b6c3941dbcc67c443ae27a67abdd1b0
alex2075 <gagara5620@gmail.com>
2023-05-17 19:57:58 +0600

    add styles for response

--- src/components/ResponseView.tsx
+++ src/components/ResponseView.tsx
@@ -7,6 +7,8 @@ type ResponsePropsType = {
 
 export const ResponseView = (props: ResponsePropsType) => {
   return (
+    <>
+    <div style={{textAlign: 'center', fontSize: 20}}><span>Response</span></div>
     <JsonFormatter
       json={JSON.stringify(props.res)}
       tabWith={4}
@@ -14,8 +16,14 @@ export const ResponseView = (props: ResponsePropsType) => {
         propertyStyle: { color: 'red' },
         stringStyle: { color: 'green' },
         numberStyle: { color: 'darkorange' },
-        style: { padding: '15px' },
+        style: { 
+          padding: '15px',
+          backgroundColor: "oldlace",
+          border: '2px solid black',
+          borderRadius: '10px'
+        },
       }}
     />
+    </>
   );
 };
